e1ce3039d3001e81f38478f9b738d0df184459bb
Blade Boles <bladerboles@gmail.com>
2019-12-11 15:32:02 -0700

    Hmm, did a lot of stuff.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const apiKey = '';
-const searchURL = 'https://developer.nps.gov/api/v1/parks';
+const apiKey = 'lrVhoFZacyq8A8SseTV0n5u4TUB4PufIHlovdnZ0';
+const searchURL = 'https://developer.nps.gov/api/v1/parks?';
 
 
 
@@ -7,25 +7,38 @@ const searchURL = 'https://developer.nps.gov/api/v1/parks';
 function getResults(stateCode, maxResults){
   //define the paramater object that we will format correctly
   const params = {
-    state: stateCode,
-    max: maxResults,
-    key: apiKey
+    stateCode: stateCode,
+    //max: maxResults,
+    api_key: apiKey
   };
 
   //call the format function so we can turn key/values to a string
   const paramString= formatQueryParam(params);
   //append the paramString to the given url 
-  const url = searchURL + '?' + paramString;
+  const url = searchURL + paramString;
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => console.log(responseJson))
+    .catch(error => console.log('error occurerrerred'));
 
 }
 
-function formatQueryParam(){
-
+function formatQueryParam(params){
+  const queryItems = Object.keys(params)
+    .map(key => `${key}=${params[key]}`);
+  return queryItems.join('&');
 }
 
 function makePage(){
-  $('form').on('submit', '#state', event => {
-    event.preventDefault;
+  $('form').submit (event => {
+    event.preventDefault();
+    console.log('event handler working');
     const stateCode = $('#state').val();
     const maxResults = $('#max-num').val();
 
@@ -33,3 +46,5 @@ function makePage(){
     getResults(stateCode, maxResults);
   });
 }
+
+$(makePage);
